d649933878a2213d03ca591f43d7ba3443fdf28a
gandalfgray <gandalfgray@yandex.ru>
2020-06-19 18:04:03 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -62,7 +62,7 @@ int __stdcall fixRefugeCamp(LoHook* hook, HookContext* c)
 
 void __stdcall ghostHeroFix(HiHook* hook, H3Army* army, int cell, H3Army* destArmy, int destCell, bool isHero,  bool destIsHero)
 {
-	if(army != destArmy)
+	if(isHero && army != destArmy)
 	{
 		creaturesCount = 0;
 		for(int i=0; i<7; i++)
