d1ec30787a20f04cea7ec109323078aed1282b75
gandalfgray <gandalfgray@yandex.ru>
2020-06-19 17:07:05 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -69,17 +69,28 @@ void __stdcall ghostHeroFix(HiHook* hook, H3Army* army, int cell, H3Army* destAr
 			creaturesCount += army->count[i];
 	}
 	
+	/*
 	destCreaturesCount = 0;
 	for(int i=0; i<7; i++)
 	{
 		if(army->type[i] >= 0)
 			destCreaturesCount += destArmy->count[i];
-	}	
+	}
+	*/
 	
+	/*
 	if ((creaturesCount <= 1) &&
 	     ((destArmy->type[destCell] == -1) || (destCreaturesCount <= 1)))
 			return;
-
+	*/
+	
+	destCellCreaturesCount = 0;
+	if(destArmy->type[destCell] >= 0)
+		destCellCreaturesCount += destArmy->count[destCell];
+	
+	if(creaturesCount <= 1 && destCellCreaturesCount == 0)
+		return;
+	
 	CALL_6(void, __thiscall, hook->GetDefaultFunc(), army, cell, destArmy, destCell, isHero, destIsHero);
 }
 
