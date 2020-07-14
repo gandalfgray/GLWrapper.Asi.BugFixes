8f8a51014d8ab477c62367cbfe86c8697aa71cd1
gandalfgray <gandalfgray@yandex.ru>
2020-07-14 13:47:57 +0300

    Update H3Structures.h

--- H3Structures.h
+++ H3Structures.h
@@ -394,61 +394,61 @@ struct H3Player
 struct H3Main
 {
  INT8 unk0[4];
- INT8 disabledShrines[70]; // * +4
- INT8 disabledSpells[70]; // * +4A
- UINT32 cs_bink; // * +90
- INT8 pTownPreSetupArray[16]; // * +94
- INT8 heroSetup[156 * 820]; // * +A4
+ INT8 disabledShrines[70]; // +4 (+0x04)
+ INT8 disabledSpells[70]; // +74 (+0x4A)
+ UINT32 cs_bink; // +144 (+0x90)
+ INT8 pTownPreSetupArray[16]; // +148 (+0x94)
+ INT8 heroSetup[156 * 820]; // +164 (+0xA4)
  INT8 unk1[4];
- INT8 campaignInfo[124]; // * +1F458
- INT8 unk2[354];
- BOOLEAN isDead[8]; // * +1F636
- WORD curr_day; // * +1F63E
- WORD curr_week; // * +1F640
- WORD curr_month; // * +1F642
+ INT8 campaignInfo[124]; // +128088 (+0x1F458)
+ INT8 unk2[354];         
+ BOOLEAN isDead[8]; // +128566 (+0x1F636)
+ WORD curr_day; // +128574 (+0x1F63E)
+ WORD curr_week; // +128576 (+0x1F640)
+ WORD curr_month; // +128578 (+0x1F642)
  INT8 unk3[32];
- INT8 artifactMerchant[28]; // * +1F664
- INT8 pBlackMarketsArray[16]; // * +1F680
- INT8 grail[8]; // * +1F690
- INT32 mapKind; // * +1F698 (0 - RoE, 1 - AB, 2 - SoD)
- BOOLEAN isCheater; // * +1F69C
- BOOLEAN inTutorial; // * +1F69D (fun fact : Griffin dwelling doesn't require Blacksmith in this mode)
+ INT8 artifactMerchant[28]; // +128612 (+0x1F664)
+ INT8 pBlackMarketsArray[16]; // +128640 (+0x1F680)
+ INT8 grail[8]; // +128656 (+0x1F690)
+ INT32 mapKind; // +128664 (+0x1F698 ) (0 - RoE, 1 - AB, 2 - SoD)
+ BOOLEAN isCheater; // +128668 (+0x1F69C)
+ BOOLEAN inTutorial; // +128669 (+0x1F69D) (fun fact : Griffin dwelling doesn't require Blacksmith in this mode)
  INT8 unk4[2];
- INT8 playersInfo[460]; // * +1F6A0
- INT8 mapInfo[720]; // * +1F86C
- H3String mapName; // * +1FB3C
- H3String mapDescription; // * +1FB4C
+ INT8 playersInfo[460]; // +128672 (+0x1F6A0)
+ INT8 mapInfo[720]; // +129132 (+0x1F86C)
+ H3String mapName; // +129852 (+0x1FB3C)
+ H3String mapDescription; // +129868 +0x1FB4C
  INT8 unk5[20];
- INT8 mainSetup[3932]; // * +1FB70
+ INT8 mainSetup[3932]; // +129904 (+0x1FB70)
  INT8 unk6[4];
- H3Player players[8]; // * +20AD0
- INT8 pTownsArray[16]; // * +21610
- H3Hero heroes[156]; // * +21620
- INT8 heroOwner[156]; // * +4DF18
- BitMaskDword heroMayBeHiredBy[156]; // * +4DFB4
- INT8 randomArtifacts[144]; // * +4E224
- INT8 artifactsAllowed[144]; // * +4E2B4
+ H3Player players[8]; // +133840 (+0x20AD0)
+ INT8 pTownsArray[16]; // +136720 (+0x21610)
+ H3Hero heroes[156]; // +136736 (+0x21620)
+ INT8 heroOwner[156]; // +319256 (+0x4DF18)
+ BitMaskDword heroMayBeHiredBy[156]; // +319412 (+0x4DFB4)
+ INT8 randomArtifacts[144]; // +320036 (+0x4E224)
+ INT8 artifactsAllowed[144]; // +320180 (+0x4E2B4)
  INT8 unk7[32];
- UINT8 keymasterVisited[8]; // * +4E364 (bitfield for players)
+ UINT8 keymasterVisited[8]; // +320356 (+0x4E364) (bitfield for players)
  INT8 unk8[12];
- INT8 pSignpostsBottlesArray[16]; // * +4E378
- INT8 pMinesLighthousesArray[16]; // * +4E388
- INT8 pDwellingsArray[16]; // * +0x4E398
- INT8 pGarrisonsArray[16]; // * +0x4E3A8
- INT8 pBoatsArray[16]; // * +0x4E3B8
- INT8 pUniversitiesArray[16]; // * +0x4E3C8
- INT8 pCreatureBanksArray[16]; // * +0x4E3D8
- INT8 obeliskCount; // * +4E3E8
- UINT8 obeliskVisited[48]; // * +4E3E9
+ INT8 pSignpostsBottlesArray[16]; // +320376 (+0x4E378)
+ INT8 pMinesLighthousesArray[16]; // +320392 (+0x4E388)
+ INT8 pDwellingsArray[16]; // +320408 (+0x4E398)
+ INT8 pGarrisonsArray[16]; // +320424 (+0x4E3A8)
+ INT8 pBoatsArray[16]; // +320440 (+0x4E3B8)
+ INT8 pUniversitiesArray[16]; // +320456 (+0x4E3C8)
+ INT8 pCreatureBanksArray[16]; // +320472 (+0x4E3D8)
+ INT8 obeliskCount; // +320488 (+0x4E3E8)
+ UINT8 obeliskVisited[48]; // +320489 +0x4E3E9
  INT8 unk9[575];
- INT8 bannedSkills[28]; // * +4E658
+ INT8 bannedSkills[28]; // +321112 (+0x4E658)
  INT8 unk10[4];
- INT8 pMonolithTwoWayArray[8 * 16]; // * +4E678 (H3Position)
- INT8 pMonolithOneWayArray[8 * 16]; // * +4E6F8 (H3Position)
+ INT8 pMonolithTwoWayArray[8 * 16]; // +321112 (+0x4E678) (H3Position)
+ INT8 pMonolithOneWayArray[8 * 16]; // +321272 (+0x4E6F8) (H3Position)
  INT8 unk11[4];
- INT8 pWhirlPoolsArray[16]; // * +4E77C (H3Position)
- INT8 pSubterraneanGatesDestinationArray[16]; // * +4E78C (H3Position)
- INT8 pSubterraneanGatesIDArray[16]; // * +4E79C (H3Position)
+ INT8 pWhirlPoolsArray[16]; // +321404 (+0x4E77C) (H3Position)
+ INT8 pSubterraneanGatesDestinationArray[16]; // +321420 (+0x4E78C) (H3Position)
+ INT8 pSubterraneanGatesIDArray[16]; // +321436 (+0x4E79C) (H3Position)
  INT8 unk_vec1[16];
  INT8 unk_vec2[16];
  INT8 unk12[4];
